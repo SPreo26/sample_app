ec3ad32abfcec050eb712c0fb859fc4aa3590ddc
spreo260@gmail.com <sergeipreobrahensky@Sergeis-MacBook-Pro.local>
2015-11-08 17:24:51 -0800

    sample_app exercise

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,10 @@
+class AccountsController < ApplicationController
+
+  def index
+  end
+
+  def create
+    puts "nnoob"
+  end
+
+end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+
+    def index
+        @time = Time.now
+        @first_number = 1
+        @second_number = 2
+        puts "Lol"
+    end
+
+    def orange_mambo
+
+    end
+
+
+end
